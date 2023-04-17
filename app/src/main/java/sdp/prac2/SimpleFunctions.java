9b5832d135500af37e9ee638448ba4e688b4971c
MashPash <122250805+MashPash@users.noreply.github.com>
2023-04-17 11:42:50 +0200

    Task1 Method

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,17 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+        // Task1
+    }
+
+    public static int Task1(int[] a, int[] b) {
+        int total = 0;
+        for (int index : b) {
+            if (index < a.length) {
+                total += a[index];
+            }
+        }
+        return total;
+    }
 }

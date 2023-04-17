534c3f9361d7ae5f6e00d218abbf20c0f354a20e
Brandon Filmer <brandonf2000@hotmail.com>
2023-04-17 12:37:05 +0200

    Made Tests for Task 4. Need method for Task 4 for proper Testing.

--- app/src/test/java/sdp/prac2/AppTest.java
+++ app/src/test/java/sdp/prac2/AppTest.java
@@ -5,6 +5,40 @@ package sdp.prac2;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
 
 class AppTest {
+
+    // Tests for Task 4 - g19f7591
+    // If lists are same length
+    @Test void Task4Equal(){
+        //Arrange
+        App classBeingTested = new App();
+        List<Integer> a = Arrays.asList(2, 4, 6, 8);
+        List<Integer> b = Arrays.asList(1, 3, 5, 7);
+
+        List<Integer> expected = Arrays.asList(14, 20, 18, 8);
+
+        //Act
+        List<Integer> result = classBeingTested.Task4(a, b);
+        
+        //Assert
+        assertIterableEquals(expected, result);
+    }
+
+    // If 2 lists are different length.
+    @Test void Task4Diff(){
+        //Arrange
+        App classBeingTested = new App();
+        List<Integer> a = Arrays.asList(2, 4, 6, 8);
+        List<Integer> b = Arrays.asList(1, 3, 5, 7,9);
+
+        //Act
+        List<Integer> result = classBeingTested.Task4(a, b);
+        
+        //Assert
+        assertIterableEquals(null, result);
+    }
+    // ---------------------------------------------------Task 4 Tests END ------------------------------------------------------------\\
+        
 }
